498be7531fe06a041915e0d9ff5e702d49972f4b
edomingues <edi.domingues@hurb.com>
2022-11-13 11:07:23 -0300

    :sparkles: added use cases to koin module

--- app/src/main/java/com/example/marmitech/marmitechUI/LoginModule.kt
+++ app/src/main/java/com/example/marmitech/marmitechUI/LoginModule.kt
@@ -1,12 +1,18 @@
 package com.example.marmitech.marmitechUI
 
 import com.example.domain.marmitech.appPeople.ILoginRepository
+import com.example.domain.marmitech.appPeople.usecase.GetFiscalSavedUseCase
+import com.example.domain.marmitech.appPeople.usecase.GetFiscalUseCase
+import com.example.domain.marmitech.appPeople.usecase.GetTurmaUseCase
 import com.example.network.weather.marmitech.appPeople.LoginRepository
 import com.example.network.weather.marmitech.appPeople.local.ILoginLocalData
 import com.example.network.weather.marmitech.appPeople.local.LoginLocalData
 import org.koin.dsl.module
 
 val loginModule = module {
-    single<ILoginRepository> {LoginRepository(localData = get(), appDatabase = get())}
-    factory<ILoginLocalData> {LoginLocalData(appDatabase = get())}
+    single<ILoginRepository> { LoginRepository(localData = get(), appDatabase = get()) }
+    factory<ILoginLocalData> { LoginLocalData(appDatabase = get()) }
+    factory { GetFiscalUseCase(repository = get()) }
+    factory { GetTurmaUseCase(repository = get()) }
+    factory { GetFiscalSavedUseCase(repository = get()) }
 }
