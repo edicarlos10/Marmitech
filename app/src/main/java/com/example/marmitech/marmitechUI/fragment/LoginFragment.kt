37629802eb6cc713198b1e49cdd126103afa2a3f
edomingues <edi.domingues@hurb.com>
2022-11-13 11:26:18 -0300

    :art: created fun in fragment login

--- app/src/main/java/com/example/marmitech/marmitechUI/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/marmitech/marmitechUI/fragment/LoginFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.marmitech.BuildConfig
+import com.example.domain.marmitech.appPeople.Fiscal
+import com.example.domain.marmitech.appPeople.Turma
+import com.example.domain.marmitech.base.Event
 import com.example.marmitech.databinding.FragmentLoginBinding
+import com.example.marmitech.marmitechUI.LoginViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginFragment : Fragment() {
-    companion object{
-        fun newInstance() : LoginFragment{
-            val args = Bundle().apply {  }
+    companion object {
+        fun newInstance(): LoginFragment {
+            val args = Bundle().apply { }
             val fragment = LoginFragment()
             fragment.arguments = args
             return fragment
@@ -20,9 +24,15 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private val loginViewModel: LoginViewModel by viewModel()
 
     override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
+
+        loginViewModel.fiscal.observe(this) { fiscal -> sucessGetFiscal(fiscal) }
+        loginViewModel.allTurmas.observe(this) { allTurmas -> sucessGetAllTurma(allTurmas) }
+        loginViewModel.loading.observe(this) { onLoading(it) }
+        loginViewModel.error.observe(this) { onError(it) }
     }
 
     override fun onCreateView(
@@ -38,4 +48,22 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    private fun sucessGetFiscal(fiscal: Fiscal?) {
+
+    }
+
+    private fun sucessGetAllTurma(allTurmas: List<Turma>?) {
+
+    }
+
+    private fun onLoading(loading: Boolean?) {
+        loading?.let {
+
+        }
+    }
+
+    private fun onError(error: Event.Error?) {
+        if (error == null)
+            return
+    }
 }
