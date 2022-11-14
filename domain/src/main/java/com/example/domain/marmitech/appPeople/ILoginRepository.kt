7da60bda01352b63491e75ee7d822a80ad85ab2c
edomingues <edi.domingues@hurb.com>
2022-11-14 13:55:34 -0300

    :art: added insert fun

--- domain/src/main/java/com/example/domain/marmitech/appPeople/ILoginRepository.kt
+++ domain/src/main/java/com/example/domain/marmitech/appPeople/ILoginRepository.kt
@@ -1,9 +1,12 @@
 package com.example.domain.marmitech.appPeople
 
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface ILoginRepository {
     fun getFiscal(matricula: Long, turma: Long): Single<Fiscal>
     fun getAllTurmas(): Single<List<Turma>>
     fun getFiscalSaved(matricula: Long, turma: Long): Single<FiscalSaved>
+    fun insertTurma(turma: Turma): Completable
+    fun insertFiscalSaved(fiscal: FiscalSaved): Completable
 }
--- network/src/main/java/com/example/network/weather/marmitech/appPeople/local/ILoginLocalData.kt
+++ network/src/main/java/com/example/network/weather/marmitech/appPeople/local/ILoginLocalData.kt
@@ -3,10 +3,13 @@ package com.example.network.weather.marmitech.appPeople.local
 import com.example.domain.marmitech.appPeople.Fiscal
 import com.example.domain.marmitech.appPeople.FiscalSaved
 import com.example.domain.marmitech.appPeople.Turma
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface ILoginLocalData {
     fun getFiscal(matricula: Long?, turma: Long?): Single<Fiscal>
     fun getAllTurmas(): Single<List<Turma>>
     fun getFiscalSaved(matricula: Long?, turma: Long?): Single<FiscalSaved>
+    fun insertTurma(turma: Turma): Completable
+    fun insertFiscalSaved(fiscaSaved: FiscalSaved): Completable
 }
