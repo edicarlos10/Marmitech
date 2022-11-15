e6c8d14cc53df90abf87571d911292bcdd8aa32d
edomingues <edi.domingues@hurb.com>
2022-11-14 21:09:15 -0300

    :art: fix entity to keep primary key and added network module

--- network/src/main/java/com/example/network/weather/marmitech/appPeople/local/database/FiscalEntity.kt
+++ network/src/main/java/com/example/network/weather/marmitech/appPeople/local/database/FiscalEntity.kt
@@ -2,12 +2,13 @@ package com.example.network.weather.marmitech.appPeople.local.database
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.domain.marmitech.appPeople.model.Fiscal
 import java.io.Serializable
 
 @Entity(tableName = "fiscal")
 data class FiscalEntity(
-    @ColumnInfo(name = "matricula") var matricula: Long? = null,
+    @PrimaryKey var matricula: Long? = null,
     @ColumnInfo(name = "senha") var senha: String? = null,
     @ColumnInfo(name = "nome") var nome: String? = null,
     @ColumnInfo(name = "turma") var turma: Int? = null,
--- network/src/main/java/com/example/network/weather/marmitech/appPeople/local/database/FiscalSavedEntity.kt
+++ network/src/main/java/com/example/network/weather/marmitech/appPeople/local/database/FiscalSavedEntity.kt
@@ -2,12 +2,13 @@ package com.example.network.weather.marmitech.appPeople.local.database
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.domain.marmitech.appPeople.model.FiscalSaved
 import java.io.Serializable
 
 @Entity(tableName = "fiscal_saved")
 data class FiscalSavedEntity(
-    @ColumnInfo(name = "matricula") var matricula: Long? = null,
+    @PrimaryKey var matricula: Long? = null,
     @ColumnInfo(name = "nome") var nome: String? = null,
     @ColumnInfo(name = "turma") var turma: Int? = null,
 ) : Serializable {
