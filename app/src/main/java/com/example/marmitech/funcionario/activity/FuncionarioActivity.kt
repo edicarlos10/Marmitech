5188832da292cb7c47d1b064364a5417df2556b9
edomingues <edi.domingues@hurb.com>
2022-11-18 16:49:10 -0300

    :sparkles: added files to represent apontamento funcionario

---
+++ app/src/main/java/com/example/marmitech/funcionario/activity/FuncionarioActivity.kt
@@ -0,0 +1,32 @@
+package com.example.marmitech.funcionario.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.marmitech.databinding.ActivityFuncionarioBinding
+import com.example.marmitech.funcionario.fragment.FuncionarioFragment
+import com.google.android.material.snackbar.Snackbar
+
+class FuncionarioActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityFuncionarioBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFuncionarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val funcionarioFragment = FuncionarioFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(binding.clFuncionarioActi.id, funcionarioFragment)
+            .commit()
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/example/marmitech/funcionario/fragment/FuncionarioFragment.kt
@@ -0,0 +1,48 @@
+package com.example.marmitech.funcionario.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marmitech.databinding.FragmentFuncionarioBinding
+
+class FuncionarioFragment : Fragment() {
+    companion object {
+        fun newInstance(): FuncionarioFragment {
+            val args = Bundle().apply { }
+            val fragment = FuncionarioFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private var _binding: FragmentFuncionarioBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFuncionarioBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
