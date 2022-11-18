d47d24246d52ec575d1ff2c5dd287e9a6ee54829
edomingues <edi.domingues@hurb.com>
2022-11-18 16:48:38 -0300

    :sparkles: added icons to main activity

--- app/src/main/java/com/example/marmitech/apontamento/activity/ApontamentoActivity.kt
+++ app/src/main/java/com/example/marmitech/apontamento/activity/ApontamentoActivity.kt
@@ -1,15 +1,27 @@
 package com.example.marmitech.apontamento.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.marmitech.databinding.ActivityLoginBinding
+import com.example.marmitech.databinding.ActivityApontamentoBinding
 import com.example.marmitech.extension.viewBinding
+import com.example.marmitech.funcionario.activity.FuncionarioActivity
+
 
 class ApontamentoActivity : AppCompatActivity() {
-    private val binding by viewBinding(ActivityLoginBinding::inflate)
+    private val binding by viewBinding(ActivityApontamentoBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.ivApontar.setOnClickListener {
+            startActivity(Intent(this, FuncionarioActivity::class.java))
+        }
+        binding.tvApontar.setOnClickListener {
+            startActivity(Intent(this, FuncionarioActivity::class.java))
+        }
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
