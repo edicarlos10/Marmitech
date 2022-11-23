de42d3d59688937cb413669c2d1829fc4b8190fa
edomingues <edi.domingues@hurb.com>
2022-11-23 20:57:37 -0300

    :art: implemented json integration

--- app/src/main/java/com/example/marmitech/extension/JsonExtension.kt
+++ app/src/main/java/com/example/marmitech/extension/JsonExtension.kt
@@ -1,9 +1,17 @@
 package com.example.marmitech.extension
 
 import android.content.Context
+import android.widget.Toast
+import com.example.domain.marmitech.appPeople.model.Apontamento
 import com.example.domain.marmitech.appPeople.model.Marmitech
+import com.fasterxml.jackson.core.JsonGenerationException
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 import java.io.IOException
 
 fun Context.readJson(jsonName: String): String {
@@ -19,4 +27,31 @@ fun Context.readJson(jsonName: String): String {
 
 fun Context.toDataList(): Marmitech {
     return jacksonObjectMapper().readValue(readJson("JsonToMarmitech.json"))
+}
+
+fun Context.toJson(it: List<Apontamento>): String {
+    return try {
+        ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(it)
+    } catch (e: JsonGenerationException) {
+        e.printStackTrace()
+        ""
+    } catch (e: JsonMappingException) {
+        e.printStackTrace()
+        ""
+    }
+}
+
+fun Context.writeFile(json: String) {
+    try {
+        val file = File(
+            this.filesDir,
+            "MarmitechToJson.json" //data/user/0/com.example.marmitech/files/MarmitechToJson.json
+        )
+        val fileWriter = FileWriter(file)
+        val bufferedWriter = BufferedWriter(fileWriter)
+        bufferedWriter.write(json)
+        bufferedWriter.close()
+    } catch (e: Exception) {
+        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+    }
 }
--- app/src/main/java/com/example/marmitech/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/marmitech/main/activity/MainActivity.kt
@@ -2,20 +2,52 @@ package com.example.marmitech.main.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.domain.marmitech.appPeople.model.Apontamento
+import com.example.domain.marmitech.base.Event
+import com.example.marmitech.apontamento.ApontamentoViewModel
 import com.example.marmitech.apontamento.activity.ConsultaActivity
 import com.example.marmitech.apontamento.activity.FuncionarioActivity
 import com.example.marmitech.databinding.ActivityApontamentoBinding
+import com.example.marmitech.extension.showDialog
+import com.example.marmitech.extension.toJson
 import com.example.marmitech.extension.viewBinding
+import com.example.marmitech.extension.writeFile
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by viewBinding(ActivityApontamentoBinding::inflate)
+    private val apontamentoViewModel: ApontamentoViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        apontamentoViewModel.loading.observe(this) { onLoading(it) }
+        apontamentoViewModel.error.observe(this) { onError(it) }
+        apontamentoViewModel.allApontamento.observe(this) { listApontamento ->
+            sucessGetApontamentos(
+                listApontamento
+            )
+        }
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setListeners()
+    }
+
+
+    private fun sucessGetApontamentos(listApontamento: List<Apontamento>?) {
+        listApontamento?.let {
+            writeFile(toJson(it))
+        } ?: kotlin.run {
+            this.showDialog("Não existe apontamentos para serem enviados.")
+        }
+    }
+
+    private fun setListeners() {
         binding.ivApontar.setOnClickListener {
             showFuncionarioIntent()
         }
@@ -30,7 +62,12 @@ class MainActivity : AppCompatActivity() {
             showConsultasIntent()
         }
 
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.tvIntegrar.setOnClickListener {
+            apontamentoViewModel.getAllApontamento()
+        }
+        binding.ivIntegrar.setOnClickListener {
+            apontamentoViewModel.getAllApontamento()
+        }
     }
 
     private fun showFuncionarioIntent() {
@@ -41,5 +78,22 @@ class MainActivity : AppCompatActivity() {
     private fun showConsultasIntent() {
         val intent = Intent(this, ConsultaActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun onLoading(loading: Boolean?) {
+        loading?.let {
+            if (loading == true) {
+                binding.clSecond.visibility = View.GONE
+                binding.progressBarMain.visibility = View.VISIBLE
+            } else {
+                binding.progressBarMain.visibility = View.GONE
+                binding.clSecond.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun onError(error: Event.Error?) {
+        if (error == null) return
+        else this.showDialog("Desculpe não foi possível completar a ação, tente novamente.")
     }
 }
